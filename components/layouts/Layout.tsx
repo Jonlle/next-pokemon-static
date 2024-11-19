16d4599fa700088466a72cfe0dee8a718d599744
Jonlle <jonlle19@gmail.com>
2024-11-19 12:54:51 -0500

    Translate meta descriptions to English for SEO optimization

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -2,44 +2,21 @@ import { FC, ReactNode } from 'react';
 import Head from 'next/head';
 
 import { NavbarApp } from '../ui/Navbar';
-import { useRouter } from 'next/router';
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const origin = typeof window === 'undefined' ? '' : window.location.origin;
-
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
       <Head>
         <title>{title || 'Pokemon app'}</title>
-        <meta
-          name='author'
-          content='Jhonatan Llerena'
-        />
-        <meta
-          name='description'
-          content={`Información sobre el pokemon ${title}`}
-        />
-        <meta
-          name='keywords'
-          content={`${title}, pokemon, pokedex, pokedex pokemon, pokedex pokemon ${title}`}
-        />
-        <meta
-          property='og:title'
-          content={`Información sobre el pokemon ${title}`}
-        />
-        <meta
-          property='og:description'
-          content={`Esta es la pagina sobre el pokemon ${title}`}
-        />
-        <meta
-          property='og:image'
-          content={`${origin}/img/banner.png`}
-        />
+        <meta name='description' content={`Information about the Pokémon ${title}`} />
+        <meta name='keywords' content={`${title}, pokemon, pokedex, pokedex pokemon, pokedex pokemon ${title}`} />
+        <meta property='og:title' content={`Information about the Pokémon ${title}`} />
+        <meta property='og:description' content={`This is the page about the Pokémon ${title}`} />
       </Head>
       <NavbarApp />
 
